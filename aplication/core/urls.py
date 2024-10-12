2f63c9c3469c3aaa9794ff1b66520f7065d3762b
El Negro Del WhatsApp <123592684+theoriginalex@users.noreply.github.com>
2024-10-12 10:22:42 -0500

    agregando clases

--- aplication/core/urls.py
+++ aplication/core/urls.py
@@ -3,12 +3,11 @@ from aplication.core import views
 
 app_name='core' # define un espacio de nombre para la aplicacion
 urlpatterns = [
-# ruta principal
-path('', views.home,name='home'),
-# ruts de doctores
 
-path('doctor_list/', views.doctor_List,name="doctor_list"),
-path('doctor_create/', views.doctor_create,name="doctor_create"),
-path('doctor_update/<int:id>/', views.doctor_update,name='doctor_update'),
-path('doctor_delete/<int:id>/', views.doctor_delete,name='doctor_delete'),
+path('', views.Home.as_view(),name='home'),
+path('doctor_list/', views.DoctorList.as_view(),name='doctor_list'),
+path('doctor_create/', views.DoctorCrear.as_view(),name="doctor_create"),
+path('doctor_update/<int:pk>/', views.DoctorUpdate.as_view(),name='doctor_update'),
+path('doctor_delete/<int:pk>/', views.DoctorDelete.as_view(),name='doctor_delete'),
+
 ]
--- aplication/core/views.py
+++ aplication/core/views.py
@@ -1,66 +1,76 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import redirect, render
-
+from django.views.generic import TemplateView, ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from aplication.core.forms import DoctorForm
 from aplication.core.models import Doctor
 
-# Create your views here.
-def home(request):
-    data={"title":"Medical","title1":"Sistema Medico Online"}
-    #return HttpResponse("<h1>Pantalla de Inicio</h1>")
-    #return JsonResponse(data)
-    return render(request,'core/home.html',data)
 
-def doctor_List(request):
-    data={"title":"Medical","title1":"Consulta de Doctores"}
-    doctores = Doctor.objects.all()
-    for doctor in doctores:
-        print(doctor.first_name," ",doctor.clinic.name)
-    data["doctores"]=doctores
-    print(data)
-    return render(request,"core/doctor/list.html",data)
+class Home(TemplateView):
+    template_name = 'core/home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Medical"
+        context['title1'] = "Sistema Medico Online"
+        return context
 
-def doctor_create(request):
-    data = {"title": "Doctores","title1": "Añadir Doctores"}
-    if request.method == "POST":
-        print(request.POST)
-        form = DoctorForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("core:doctor_list")
-        else:
-            data["form"] = form
-            data["error"] = "Error al crear el Doctor."
-        return render(request, "core/doctor/form.html", data)
-    else:
-        form = DoctorForm()
-        data["form"] = form
-    print(form)
-    return render(request, "core/doctor/form.html", data)
 
-def doctor_update(request,id):
-    data = {"title": "Doctores","title1": "Editar Doctor"}
-    doctor = Doctor.objects.get(pk=id)# doctor1
-    if request.method == "POST":
-        form = DoctorForm(request.POST,instance=doctor)
-        if form.is_valid():
-            form.save()
-            return redirect("core:doctor_list")
-        else:
-            data["form"] = form
-            data["error"] = "Error al editar el Doctor."
-            return render(request, "core/doctor/form.html", data)
-    else:
-        form = DoctorForm(instance=doctor)
-        data["form"] = form
-        print(form)
-    return render(request, "core/doctor/form.html", data)
+class DoctorList(ListView):
+    model = Doctor
+    template_name = 'core/doctor/list.html'
+    context_object_name = 'doctores'
 
-#@login_required
-def doctor_delete(request,id):
-    doctor = Doctor.objects.get(id=id)
-    data = {"title":"Eliminar","title1":"Eliminar Doctor","doctor":doctor}
-    if request.method == "POST":
-        doctor.delete()
-        return redirect("core:doctor_list")
-    return render(request, "core/doctor/delete.html", data)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Medical"
+        context['title1'] = "Consulta de Doctores"
+        return context
+
+class DoctorCrear(CreateView):
+    model = Doctor
+    form_class = DoctorForm
+    template_name = 'core/doctor/form.html'
+    success_url = reverse_lazy('core:doctor_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Doctores"
+        context['title1'] = "Añadir Doctores"
+        return context
+
+    def form_invalid(self, form):
+        context = self.get_context_data()
+        context['form'] = form
+        context['error'] = "Error al crear el Doctor."
+        return self.render_to_response(context)
+
+
+class DoctorUpdate(UpdateView):
+    model = Doctor
+    form_class = DoctorForm
+    template_name = 'core/doctor/form.html'
+    success_url = reverse_lazy('core:doctor_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Doctores"
+        context['title1'] = "Editar Doctor"
+        return context
+
+    def form_invalid(self, form):
+        context = self.get_context_data()
+        context['form'] = form
+        context['error'] = "Error al editar el Doctor."
+        return self.render_to_response(context)
+
+
+class DoctorDelete(DeleteView):
+    model = Doctor
+    template_name = 'core/doctor/delete.html'
+    success_url = reverse_lazy('core:doctor_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Eliminar"
+        context['title1'] = "Eliminar Doctor"
+        return context
+
